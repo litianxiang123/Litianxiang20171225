df5ccac5473d4cdb04e91187e237cae9de2b6125
litianxiang123 <2514943287@qq.com>
2017-12-25 14:01:26 +0800

    第三次添加

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,8 @@
 package helloworld.example.com.litianxiang20171225;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     //视频
     public void video(){
+
+    }
+
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
 
     }
 }
