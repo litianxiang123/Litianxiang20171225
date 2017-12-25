d14a7363607cccc9907867441b21e0388e3e8d81
litianxiang123 <2514943287@qq.com>
2017-12-25 14:13:03 +0800

    第四次添加

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,27 @@
 package helloworld.example.com.litianxiang20171225;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
     }
 
     //视频
-    public void video(){
+    public void video() {
 
+    }
+
+    private void initView() {
+        mTv = (TextView) findViewById(R.id.tv);
     }
 }
